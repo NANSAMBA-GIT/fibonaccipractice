233f053deddf3d5a69acdc22221462224e0488e4
olivia <lwangaolivia20@gmail.com>
2025-04-03 23:38:43 +0300

    testfirst

---
+++ fibonnace.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestFibonacci(unittest.TestCase):
+    def test_valid_fibonacci(self):
+        self.assertEqual(fibcalc(0, 1, 6), [0, 1, 1, 2, 3, 5, 8])  # 7 terms
+        self.assertEqual(fibcalc(2, 3, 4), [2, 3, 5, 8, 13])  # 5 terms
+        self.assertEqual(fibcalc(5, 8, 5), [5, 8, 13, 21, 34, 55])  # 6 terms
+
+
+if __name__ == "__main__":
+    unittest.main()
