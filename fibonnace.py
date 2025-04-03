57e3a139e8a73399de594472ee3406c82a609d40
olivia <lwangaolivia20@gmail.com>
2025-04-04 00:00:03 +0300

    successfultest

--- fibonnace.py
+++ fibonnace.py
@@ -1,7 +1,18 @@
 import unittest
 
 
+def fibcalc(first, second, length):
+
+    if length == 0:
+        return [first]
+
+    sequence = [first]  # Start with the first value
+    sequence += fibcalc(second, first + second, length - 1)  # Recursively app
+    return sequence
+
+
 class TestFibonacci(unittest.TestCase):
+
     def test_valid_fibonacci(self):
         self.assertEqual(fibcalc(0, 1, 6), [0, 1, 1, 2, 3, 5, 8])  # 7 terms
         self.assertEqual(fibcalc(2, 3, 4), [2, 3, 5, 8, 13])  # 5 terms
